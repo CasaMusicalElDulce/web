eb0f406a483037f80e6947b41889c758d0b0d5c8
CasaMusicalElDulce <waldimartello8@gmail.com>
2024-12-17 20:30:31 -0500

    Add files via upload

---
+++ ARBOLITO NAVIDAD/navidad.js
@@ -0,0 +1,17 @@
+let hojas="";
+[20,1,1,3].forEach(function(a){
+    Array(a).fill("").forEach(function(x,i){
+        hojas+=([...Array(1+i).fill("<span class='colores'>*~*</span>")].join(""))+"<br>"
+
+    })
+})
+document.querySelector(" .arbol").innerHTML=hojas;
+//vamos ponerle colores
+let colores=document.querySelectorAll(".colores");
+function animacion(){
+    for(let i=0; i<colores.length;i++){
+        let tiempo=i/100+1;
+        colores[i].style.animation="color "+tiempo+"s infinite";
+    }
+}
+window.addEventListener("load", animacion)
